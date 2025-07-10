a4792db46d2a74e2f543a9809c08a489d06a93d7
Uchechukwu Alih <alihuchechukwumichael@gmail.com>
2025-07-09 23:10:28 -0500

    config base

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/personal-website/',
+  base:'/personal_website/',
   plugins: [react(), tailwindcss()],
 })
