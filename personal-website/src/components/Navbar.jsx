53e4a7338aaf4913348724be1dce3a95aaac4567
Uchechukwu Alih <alihuchechukwumichael@gmail.com>
2025-07-27 22:50:15 -0500

    removed link entirely, using only id's

--- personal-website/src/components/Navbar.jsx
+++ personal-website/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 // import '../index.css'
 
 const Navbar = () => {
     return (
         <nav className="sticky top-0 z-50 flex justify-between items-center p-4 bg-slate-900/80 text-white backdrop-blur-sm border-b border-slate-700">
             <div className="navbar-left">
-                <Link to="/" className="text-2xl font-bold text-shite hover:text-red-500 transition-colors">Uche</Link>
+                <a href="#home" className="text-2xl font-bold text-shite hover:text-red-500 transition-colors">Uche</a>
             </div>
             <div className="navbar-center">
                 <ul className="flex items-center gap-6 text-lg">
